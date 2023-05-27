33ee1a9d8134ad5abbe0bf29ca8a85f73f6ddd6d
Gulleid <gulleidmohamed93@gmail.com>
2023-05-27 15:07:00 -0800

    Calculator Asiggement

--- challenge.js
+++ challenge.js
@@ -1,2 +1,11 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+function calculator(num1, num2){
+    let add = num1+num2;
+    let mult= num1*num2;
+    let subt = num1-num2;
+    let div = num1/num2;
+    return {add, mult,subt , div};
+}
+
+console.log(calculator(5,3));
